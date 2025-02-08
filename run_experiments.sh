e7b4e63b6b1ec9d130b2144f7c845bf6988ccfc7
Matthew Scott <m.valckescott@gmail.com>
2025-02-07 17:17:19 -0800

    low memory test

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,7 +7,7 @@ VENV_ACTIVATE_PATH="/home/mscott99/projects/def-oyilmaz/mscott99/Sparse_adapted_
 ACCOUNT="def-oyilmaz"
 JOB_NAME="jan_2025"
 TIME="02:00:00"    # Max export SBATCH_ACCOUNTexpected time for each job
-MEMORY="2G"      # Max expected memory for each job
+MEMORY="2M"      # Max expected memory for each job
 CPU_NUM="1"
 ROWS_PER_WORKER=5 # 50 ends in 30 ish minutes
 
